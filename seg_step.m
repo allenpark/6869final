0aabcdb9f6b334234d76459c44c90b1248c65d51
Allen Park <allenpark@mit.edu>
2014-12-09 02:13:09 -0500

    cleaned up seg_step and added 6_256

--- seg_step.m
+++ seg_step.m
@@ -15,9 +15,6 @@ if compi ~= compj
     mint = MInt(compi, compj, seg, im);
     if w <= mint
         % merge compi and compj
-%         mincomp = min(compi, compj);
-%         maxcomp = max(compi, compj);
-%         compsz = size(seg);
         if size(seg{compi, 1}, 2) > size(seg{compj, 1}, 2)
             smaller_comp = compj;
             larger_comp = compi;
@@ -37,17 +34,5 @@ if compi ~= compj
         seg{larger_comp, 2} = w;
         seg{smaller_comp, 1} = [];
         seg{smaller_comp, 2} = -1;
-%         new_comp = cell(1, 2);
-%         new_comp{1} = [seg{compi, 1} seg{compj, 1}];
-%         new_comp{2} = max(w, max(Int(compi, seg, im), Int(compj, seg, im)));
-%         next = cell(compsz(1)-1, 2);
-%         next(1:mincomp-1, :) = seg(1:mincomp-1, :);
-%         next(mincomp:maxcomp-2, :) = seg(mincomp+1:maxcomp-1, :);
-%         next(maxcomp-1:compsz(1)-2, :) = seg(maxcomp+1:compsz(1), :);
-%         next(compsz(1)-1, :) = new_comp;
-%         seg = next;
-        %next = {seg{1:mincomp-1, :} seg{mincomp+1:maxcomp-1, :} seg{maxcomp+1:compsz(1), :} new_comp};
-    elseif w < 10
-        %fprintf('Not merging %d and %d: w=%d mint=%d inti=%d intj=%d size(compi)=%d size(compj)=%d\n', compi, compj, w, round(mint), Int(compi, seg, im), Int(compj, seg, im), length(seg{compi, 1}), length(seg{compj, 1}));
     end
 end
